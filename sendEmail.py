af81c8c7af91c325b3fe62b5ab7f4d0ac367ca0b
Hacıağa Sadıxov <hajiaghasadikhov@gmail.com>
2025-05-07 10:15:06 +0400

    Update sendEmail.py

--- sendEmail.py
+++ sendEmail.py
@@ -3,7 +3,7 @@ import smtplib, ssl, os, asyncio
 
 load_dotenv()
 SMTP_HOST = os.getenv("SMTP_HOST")
-SMTP_PORT = int(os.getenv("SMTP_PORT"))
+SMTP_PORT = os.getenv("SMTP_PORT")
 SMTP_USER = os.getenv("SMTP_USER")
 SMTP_PASS = os.getenv("SMTP_PASS")
 context = ssl.create_default_context()
